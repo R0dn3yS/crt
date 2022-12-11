419b4234f274bc80609732789c8c72f74b53a2c5
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-12-11 03:05:17 +0100

    it fast, but where vsync

--- src/display.rs
+++ src/display.rs
@@ -1,6 +1,4 @@
-use std::path::Path;
-
-use image::{GenericImageView, Pixel, Rgb};
+use image::{GenericImageView, Pixel, Rgb, DynamicImage};
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::Canvas;
@@ -9,34 +7,30 @@ use sdl2::video::Window;
 pub struct Display {
   pub width: u32,
   pub height: u32,
-  pub fps: u8,
   pub scanline_h: u32,
   pub scanline_v: u32,
 }
 
 impl Display {
-  pub fn new(width: u32, height: u32, fps: u8) -> Self {
+  pub fn new(width: u32, height: u32) -> Self {
     Display {
       width,
       height,
-      fps,
       scanline_h: 0,
       scanline_v: 0,
     }
   }
 
-  pub fn draw_screen(&mut self, canvas: &mut Canvas<Window>, path: &str) {
-    let pixel = self.get_pixel(path);
+  pub fn draw_screen(&mut self, canvas: &mut Canvas<Window>, img: &DynamicImage) {
+    let pixel = self.get_pixel(img);
 
     canvas.set_draw_color(Color::RGB(pixel[0], pixel[1], pixel[2]));
-    let rect = Rect::new(self.scanline_h as i32, self.scanline_v as i32, 1, 1);
+
+    let rect = Rect::new(self.scanline_h as i32, self.scanline_v as i32, 0, 0);
     canvas.fill_rect(rect).unwrap();
-    canvas.present();
   }
 
-  fn get_pixel(&self, path: &str) -> Rgb<u8> {
-    let img = image::open(&Path::new(path)).unwrap();
-
+  fn get_pixel(&self, img: &DynamicImage) -> Rgb<u8> {
     img.get_pixel(self.scanline_h, self.scanline_v).to_rgb()
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 mod display;
 
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
+use std::path::Path;
 
 use display::Display;
 
 fn main() {
-  let mut crt: Display = Display::new(1280, 720, 25);
+  let mut crt: Display = Display::new(1280, 720);
 
   let sdl_context = sdl2::init().unwrap();
   let video_subsystem = sdl_context.video().unwrap();
@@ -17,46 +16,30 @@ fn main() {
     .build()
     .unwrap();
 
-  let mut canvas = window.into_canvas().index(find_sdl_gl_driver().unwrap()).present_vsync().build().unwrap();
+  let mut canvas = window.into_canvas().build().unwrap();
   canvas.clear();
   canvas.present();
 
-  let mut event_pump = sdl_context.event_pump().unwrap();
   let mut frame_file = 1;
+  let mut file_path = ("/home/rodney/.video/".to_string() + &pad_start(frame_file.to_string(), 6, "0") + ".png").to_owned();
+  let mut img = image::open(&Path::new(&file_path)).unwrap();
 
-  'crtloop: loop {
-    for evt in event_pump.poll_iter() {
-      match evt {
-        Event::Quit{..} | Event::KeyDown{keycode: Some(Keycode::Escape), ..} => {
-          break 'crtloop
-        },
-        _ => ()
-      }
-    }
-
-    let file_path = ("/home/rodney/.video/".to_string() + &pad_start(frame_file.to_string(), 6, "0") + ".png").to_owned();
-
-    crt.draw_screen(&mut canvas, &file_path);
+  loop {
+    crt.draw_screen(&mut canvas, &img);
 
     crt.scanline_h += 1;
     if crt.scanline_h == crt.width {
       crt.scanline_h = 0;
       crt.scanline_v += 1;
+      canvas.present();
       if crt.scanline_v == crt.height {
         crt.scanline_v = 0;
         frame_file += 1;
+        file_path = ("/home/rodney/.video/".to_string() + &pad_start(frame_file.to_string(), 6, "0") + ".png").to_owned();
+        img = image::open(&Path::new(&file_path)).unwrap();
       }
     }
   }
-}
-
-fn find_sdl_gl_driver() -> Option<u32> {
-  for (index, item) in sdl2::render::drivers().enumerate() {
-    if item.name == "opengl" {
-      return Some(index as u32);
-    }
-  }
-  None
 }
 
 fn pad_start(text: String, length: u8, fill: &str) -> String {
